e5f6b34126d9e8d3481ff35a78eeaf4a16df532b
Balla Keerthi <keerthiballa@gmail.com>
2021-11-07 20:01:05 -0500

    Exercise 2 halfway through. Deleted locked file to allow for git addition

---
+++ tree.py
@@ -0,0 +1,56 @@
+"""
+Exercise 2
+Create a program called tree.py
+
+Given a file path (absolute or relative), the program should write to a file all of the contents of the directory and the child directories bellow it. The output file should look something like this:
+
+./file1.py
+./file2.py
+./dir1/file1_in_dir1.txt
+./dir1/file2_in_dir1.txt
+./dir3/file1_in_dir3.txt
+
+import os
+import pathlib
+
+file_path = input('Enter file path:')
+path_type = input('Enter abs or rel:')
+
+try:
+    if os.path.isfile(file_path):
+        os.chdir(file_path)
+        os.listdir(file_path)
+
+
+    if path_type == 'abs':
+        path = pathlib.PurePath(file_path)
+        directory = os.path.abspath(path)
+        #for i in directory:
+         #   print(i)
+    parent_dir = os.path.dirname(path)
+    print(parent_dir)
+
+
+
+
+except (NotADirectoryError, FileNotFoundError):
+    print('Not a file or there is no such file')
+"""
+
+import os
+import pathlib
+
+file_path = input('Enter file path:')
+dir_name_1 = os.path.dirname(file_path)
+dir_name = os.chdir(dir_name_1)
+
+if os.path.isfile(file_path):
+
+    for root, dirs, files in os.walk(".", topdown=True):
+        for name in files:
+            print(os.path.join(root,name))
+        for name in dirs:
+            print(os.path.join(root,name))
+
+else:
+    print('This is not a file path')
